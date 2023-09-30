032cb3c6603a3800d94b4782d0c1cd86df0e5e39
Kaushik Chowdhury <95243692+darkard2003@users.noreply.github.com>
2023-09-30 10:20:19 +0530

    Removed lazy lock

--- lua/plugins/indent-backline.lua
+++ lua/plugins/indent-backline.lua
@@ -6,7 +6,7 @@ return {
         vim.opt.listchars:append "space:⋅"
         vim.opt.listchars:append "eol:↴"
 
-        require("indent_blankline").setup {
+        require("ibl").setup {
             space_char_blankline = " ",
             show_current_context = true,
             show_current_context_start = true,
