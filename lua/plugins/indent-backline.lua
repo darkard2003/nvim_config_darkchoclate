8a474051401c68fcec433bde1eed64993cbb7bb5
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2023-10-07 23:49:48 +0530

    Changed inddent backling

--- lua/plugins/indent-backline.lua
+++ lua/plugins/indent-backline.lua
@@ -1,16 +1,2 @@
-return {
-    "lukas-reineke/indent-blankline.nvim",
+return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
 
-    config = function()
-        vim.opt.list = true
-        vim.opt.listchars:append "space:⋅"
-        vim.opt.listchars:append "eol:↴"
-
-        require("ibl").setup {
-            space_char_blankline = " ",
-            show_current_context = true,
-            show_current_context_start = true,
-        }
-    end
-
-}
