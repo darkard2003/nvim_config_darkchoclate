06048b8bc227837b236e1586c0b4e4de8c875ce1
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2023-11-25 21:03:52 +0530

    Improved readme file

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -1,30 +1,31 @@
 return {
-    'akinsho/flutter-tools.nvim',
-    lazy = true,
-    ft = { 'dart' },
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim',
-    },
-    config = function()
-        local m = require "configs.lsp.handler"
-        require('flutter-tools').setup {
-            lsp = {
-                on_attach = m.on_attach,
-                capabilities = m.capabilities,
-            },
-            decorations = {
-                statusline = {
-                    app_version = true,
-                    device = true,
-                },
-            },
-            widget_guides = {
-                enabled = true,
-            },
-        }
+  'akinsho/flutter-tools.nvim',
+  lazy = true,
+  ft = { 'dart' },
+  priority = 500,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim',
+  },
+  config = function()
+    local m = require "configs.lsp.handler"
+    require('flutter-tools').setup {
+      lsp = {
+        on_attach = m.on_attach,
+        capabilities = m.capabilities,
+      },
+      decorations = {
+        statusline = {
+          app_version = true,
+          device = true,
+        },
+      },
+      widget_guides = {
+        enabled = true,
+      },
+    }
 
-        require("telescope").load_extension("flutter")
-        vim.keymap.set('n', "<leader>fc", ":Telescope flutter commands<CR>", { silent = true })
-    end,
+    require("telescope").load_extension("flutter")
+    vim.keymap.set('n', "<leader>fc", ":Telescope flutter commands<CR>", { silent = true })
+  end,
 }
