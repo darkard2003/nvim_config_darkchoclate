a80a4277722fdcaff948acb0d2234ccce4c53aa5
darkard2003 <kaushikchowdhury231@gmail.com>
2024-10-23 01:01:48 +0530

    Customized silicon

--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -30,11 +30,20 @@ else
   return {
     'krivahtoo/silicon.nvim',
     build = './install.sh',
+    theme = 'Cappuccin',
     config = function()
       require('silicon').setup({
         theme = 'Monokai Extended',
+        font = 'JetBrainsMono Nerd Font',
+        window_title = function ()
+          return "DarkChoclate"
+        end,
         output = {
-          clipboard = false,
+          -- clipboard = false,
+        },
+        watermark = {
+          text = "@darkard2003",
+          style = "bold"
         }
       })
     end
