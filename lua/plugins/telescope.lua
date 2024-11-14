470284ea36f5d9768f59a91d5420f6ae3e08d80f
darkard2003 <kaushikchowdhury231@gmail.com>
2024-11-15 01:20:10 +0530

    removed fm binding from telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>fc', builtin.commands, {})
-        vim.keymap.set('n', '<leader>fr', builtin.registers, {})
-        vim.keymap.set('n', '<leader>fm', builtin.marks, {})
+        -- vim.keymap.set('n', '<leader>fr', builtin.registers, {})
+        -- vim.keymap.set('n', '<leader>fm', builtin.marks, {})
     end
 }
